0fe39b7ad2fe922c58e88c86e646c50253bd1ab5
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-16 12:59:31 +0200

    Adjust consumer and student model for json serialization

--- src/main/java/com/example/kafkademo/consumer/KafkaConsumer.java
+++ src/main/java/com/example/kafkademo/consumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.example.kafkademo.consumer;
 
+import com.example.kafkademo.payload.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -7,8 +8,13 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class KafkaConsumer {
-    @KafkaListener(topics = "topicNew", groupId = "myGroup")
+    // @KafkaListener(topics = "topicNew", groupId = "myGroup")
     public void consumeMessage(String message) {
         log.info(String.format("Consuming message from topic100:: %s", message));
+    }
+
+    @KafkaListener(topics = "topicNew", groupId = "myGroup")
+    public void consumeJsonMessage(Student student) {
+        log.info(String.format("Consuming message from topicNew:: %s", student.toString()));
     }
 }
--- src/main/java/com/example/kafkademo/payload/Student.java
+++ src/main/java/com/example/kafkademo/payload/Student.java
@@ -2,9 +2,11 @@ package com.example.kafkademo.payload;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Student {
     private int id;
     private String firstName;
